8a459a2011165b6ed43ec70700aa5134108fb606
kevin roy <kevnroy1@gmail.com>
2025-06-19 18:32:43 +0530

    adding error handling in data access layer

--- src/lib/dal.ts
+++ src/lib/dal.ts
@@ -1,64 +1,83 @@
 import { cache } from "react";
 
 export const fetchCountries = cache(async () => {
-  const res = await fetch(
-    "https://restcountries.com/v3.1/all?fields=name,flags,population,region,capital,cca2",
-    {
-      next: { revalidate: 86400 }, // 24 hours in seconds
-    }
-  );
-  if (!res.ok) throw new Error("Failed to fetch countries");
-  return res.json();
+  try {
+    const res = await fetch(
+      "https://restcountries.com/v3.1/all?fields=name,flags,population,region,capital,cca2",
+      {
+        next: { revalidate: 86400 }, // 24 hours in seconds
+      }
+    );
+    if (!res.ok) throw new Error("Failed to fetch countries");
+    return res.json();
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 });
 
 export const fetchCountryByCode = cache(async (code: string) => {
-  const res = await fetch(
-    `https://restcountries.com/v3.1/alpha/${code}?fields=name,flags,population,region,subregion,capital,tld,currencies,languages,borders,cca2`,
-    {
-      next: { revalidate: 86400 }, // 1 day
+  try {
+    const res = await fetch(
+      `https://restcountries.com/v3.1/alpha/${code}?fields=name,flags,population,region,subregion,capital,tld,currencies,languages,borders,cca2`,
+      {
+        next: { revalidate: 86400 }, // 1 day
+      }
+    );
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch country");
     }
-  );
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch country");
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return [];
   }
-
-  const data = await res.json();
-  return data;
 });
 
 export const fetchBorderCountries = cache(async (codes: string[]) => {
   if (!codes.length) return [];
+  try {
+    const res = await fetch(
+      `https://restcountries.com/v3.1/alpha?codes=${codes.join(
+        ","
+      )}&fields=name,cca2`,
+      {
+        next: { revalidate: 86400 },
+      }
+    );
 
-  const res = await fetch(
-    `https://restcountries.com/v3.1/alpha?codes=${codes.join(
-      ","
-    )}&fields=name,cca2`,
-    {
-      next: { revalidate: 86400 },
+    if (!res.ok) {
+      throw new Error("Failed to fetch border countries");
     }
-  );
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch border countries");
+    return res.json();
+  } catch (error) {
+    console.error(error);
+    return [];
   }
-
-  return res.json();
 });
 
 export const fetchCountriesByCodes = cache(async (codes: string[]) => {
-  const res = await fetch(
-    `https://restcountries.com/v3.1/alpha?codes=${codes.join(
-      ","
-    )}&fields=name,cca2`,
-    {
-      next: { revalidate: 86400 },
+  try {
+    const res = await fetch(
+      `https://restcountries.com/v3.1/alpha?codes=${codes.join(
+        ","
+      )}&fields=name,cca2`,
+      {
+        next: { revalidate: 86400 },
+      }
+    );
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch countries by codes");
     }
-  );
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch countries by codes");
+    return res.json();
+  } catch (error) {
+    console.error(error);
+    return [];
   }
-
-  return res.json();
 });
