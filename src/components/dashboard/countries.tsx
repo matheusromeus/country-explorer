7f5e7732966685206a82ffbd5f792a4654284364
kevin roy <kevnroy1@gmail.com>
2025-06-20 20:20:53 +0530

    better error handling

--- src/components/dashboard/countries.tsx
+++ src/components/dashboard/countries.tsx
@@ -3,12 +3,20 @@ import { Country } from "@/types/countries";
 import { DataTable } from "./data-table";
 
 const Countries = async () => {
-  const countries: Country[] = await fetchCountries();
-  return (
-    <div className="p-10">
-      <DataTable countries={countries} />
-    </div>
-  );
+  try {
+    const countries: Country[] = await fetchCountries();
+    return (
+      <div className="p-10">
+        <DataTable countries={countries} />
+      </div>
+    );
+  } catch (error) {
+    return (
+      <div className="h-screen flex items-center justify-center">
+        Failed to load countries. Please try again later.
+      </div>
+    );
+  }
 };
 
 export default Countries;
