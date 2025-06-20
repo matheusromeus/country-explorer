ad3cacbf7d0b87324d05a01897eeb8e9673cd883
kevin roy <kevnroy1@gmail.com>
2025-06-20 19:20:52 +0530

    loader for individual country

--- src/app/country/[code]/loading.tsx
+++ src/app/country/[code]/loading.tsx
@@ -1,3 +1,12 @@
+import { Loader2 } from "lucide-react";
+
 export default function Loading() {
-  return <div>Loading...</div>;
+  return (
+    <div className="relative flex flex-col gap-4 justify-center items-center min-h-screen">
+      <Loader2 className="w-12 h-12 animate-spin" />
+      <p className="text-lg text-gray-900 dark:text-gray-100">
+        What a beautiful country!
+      </p>
+    </div>
+  );
 }
